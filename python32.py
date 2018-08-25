1e65e3f03a6b2b32d62f9a4ee56a8dd2fbe0aac9
Gdykeman <gerald.dykeman@gmail.com>
2018-08-25 16:35:15 -0400

    new scripts

--- python32.py
+++ python32.py
@@ -1,7 +1,7 @@
 import getpass
 import telnetlib
 
-HOST = "172.16.1.10"
+HOST = "172.16.1.30"
 user = input("Enter your telnet username: ")
 password = getpass.getpass()
 
@@ -13,16 +13,16 @@ if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
-#tn.write(b"enable\n")
-#tn.write(b"cisco\n")
 tn.write(b"conf t\n")
-tn.write(b"int loop 0\n")
-tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
-tn.write(b"int loop 1\n")
-tn.write(b"ip address 2.2.2.2 255.255.255.255\n")
-tn.write(b"router ospf 1\n")
-tn.write(b"network 0.0.0.0 255.255.255.255 area 0\n")
+tn.write(b"vlan 2\n")
+tn.write(b"name PYTHON_VLAN_2\n")
+tn.write(b"vlan 3\n")
+tn.write(b"name PYTHON_VLAN_3\n")
+tn.write(b"vlan 4\n")
+tn.write(b"name PYTHON_VLAN_4\n")
+tn.write(b"vlan 5\n")
+tn.write(b"name PYTHON_VLAN_5\n")
 tn.write(b"end\n")
 tn.write(b"exit\n")
 
-print(tn.read_all().decode('ascii'))  
+print(tn.read_all().decode('ascii'))
--- python33.py
+++ python33.py
@@ -1,7 +1,7 @@
 import getpass
 import telnetlib
 
-HOST = "172.16.1.10"
+HOST = "172.16.1.30"
 user = input("Enter your telnet username: ")
 password = getpass.getpass()
 
@@ -13,16 +13,14 @@ if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
-#tn.write(b"enable\n")
-#tn.write(b"cisco\n")
 tn.write(b"conf t\n")
-tn.write(b"int loop 0\n")
-tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
-tn.write(b"int loop 1\n")
-tn.write(b"ip address 2.2.2.2 255.255.255.255\n")
-tn.write(b"router ospf 1\n")
-tn.write(b"network 0.0.0.0 255.255.255.255 area 0\n")
+
+for vlan in range(2,10):
+    tn.write(b"vlan " + str(vlan).encode('ascii') + b"\n")
+    tn.write(b"name PYTHON_VLAN_" + str(vlan).encode('ascii') + b"\n")
+
 tn.write(b"end\n")
+tn.write(b"wr\n")
 tn.write(b"exit\n")
 
-print(tn.read_all().decode('ascii'))  
+print(tn.read_all().decode('ascii'))
